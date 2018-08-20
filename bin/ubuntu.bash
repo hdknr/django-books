220c8c4ef5556c266d92f848be3ce9ae721aabf6
hdknr <gmail@hdknr.com>
2018-08-21 04:40:03 +0900

    tools

---
+++ bin/ubuntu.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+PKGS=(
+    build-essential
+    curl
+    libbz2-dev
+    libreadline-dev
+    libsqlite3-dev
+    libssl-dev
+    llvm
+    make
+    wget
+    zlib1g-dev
+    libmysqlclient-dev
+    mysql-server
+    pkg-config
+    graphviz
+    graphviz-dev
+    libgraphviz-dev
+    unzip
+    libfreetype6
+    libfreetype6-dev
+    supervisor
+    memcached
+    libmemcached-dev
+    libmemcached-tools
+    nginx-full
+    bsdtar
+    inotify-tools
+)
+sudo apt-get update && sudo apt-get install -y  "${PKGS[@]}"
